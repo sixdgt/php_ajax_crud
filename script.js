f84aa083b4084809e8c1533bfb5bd5559d786991
sixdgt <c4crypt@gmail.com>
2024-04-04 11:00:20 +0545

    ajax data insert

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-    
+    $('#kycForm').on('submit', function(e){
+        e.preventDefault();
+        let full_name = $('#full_name').val();
+        let email = $('#email').val();
+        let contact = $('#contact').val();
+        let address = $('#address').val();
+        let password = $('#password').val();
+
+        $.ajax({
+            type: "POST",
+            url: "post_data.php",
+            dataType: "application/json",
+            data: {
+                full_name: full_name,
+                email: email,
+                contact: contact,
+                address: address,
+                password: password
+            },
+            success: function(response){
+                console.log(JSON.parse(JSON.stringify(response)));
+            }
+        });
+
+    });
 });
